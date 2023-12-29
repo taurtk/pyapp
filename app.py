516847069c9aaedbdd2c56311d3d9c0b86e9f9c4
taurtk <taurtk@gmail.com>
2023-12-29 17:52:25 +0530

    first step

--- app.py
+++ app.py
@@ -1,10 +1,69 @@
 from flask import Flask, render_template, request
-
+from prompts import step1
 app = Flask(__name__)
+import openai
+from flask import request
+
+
+openai.api_key = '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'
+
+def prompts_for_analysis():
+    prompts = [step1
+           ]
+    return prompts
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/generate_feedback', methods=['POST'])
+def generate_feedback():
+    # Define a list to store user inputs
+    user_inputs = [request.form[f'user_input{i}'] for i in range(1, 5)]
+    print(len(user_inputs))
+    user_input = user_inputs[0]
+    user_input2 = user_inputs[1]
+    user_input3 = user_inputs[2]
+    user_input4 = user_inputs[3]
+
+    step1 = f"""Here are some notes on a presentation we are preparing. We think people believe the following now:
+    |{user_input3} After our talk we want them to believe
+    |{user_input4}
+    The end objective of this pitch is
+    |{user_input2} Based on these notes does it appear we have a clear objective for our presentation"""
+   
+    print(step1)
+    generated_feedback = []
+
+    # Loop through prompts and get responses from ChatGPT
+    for prompt in prompts_for_analysis():
+        # Combine user input and current prompt
+        # combined_prompt = f"{prompt}\n\nUser's Input:\n{user_inputs}"
+
+        # print(combined_prompt)
+        # Call OpenAI API
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",  # You can choose a different engine
+            messages=[
+            {"role": "user", "content": step1}
+        ],
+            max_tokens=150
+        )
+    
+    print(response)
+        # Append the response to the list
+    feedback =  response.choices[0].message.content
+    print(feedback)
+    
+    # Call GPT-3 model to generate feedback (replace 'your_openai_api_key')
+    # You can use the OpenAI GPT-3 Python library to interact with the API
+    # For example, you can use OpenAI's `openai.Completion.create` method
+    # to generate feedback based on user inputs
+    # feedback = openai.Completion.create(model="text-davinci-002", prompt=user_inputs, ...)
+    #  = "Generated feedback from GPT-3 goes here."
+    
+    return render_template('index.html', user_inputs=user_inputs, feedback=feedback)
+
 
 @app.route('/greet', methods=['POST'])
 def greet():
---
+++ prompts.py
@@ -0,0 +1,75 @@
+step1="""Here are 
+some notes on a presentation we are preparing We 
+think people believe the following now 
+|{user_input3} After our talk we want them to believe 
+|{user_input4}
+The end objective of this pitch is 
+|{user_input2} Based on the these notes does it appear we 
+ have a clear objective for our presentation"""
+
+
+
+
+# step12 = """Here is the introduction of a
+#  pitch we are planning to make 
+#  | Pregnancy is hard enough, we make it easiers with 
+#  our app by Pregnancy and Period
+# calculating app which according to the dates
+#  will provide discounts on recommend products
+#    eg. self hygience, protiens, vitamins etc  
+#    Free periods and pregancy calculator
+#      Discounts based on your dates of 
+#      periods and stage of pregnancy Free educational content 
+#      | Is this introduction clear, compelling and interesting ?"""
+
+
+
+
+# step21 ="""Here are 3 parts of a presenation we 
+# are are working on  We aim to solve this "problem' 
+# |How to maintain self hygiene and health during 
+
+# Pregnancy and Periods  The benefit we offer is 
+# |To provide discounted products for wellness  
+# The reason to believe is | partnership with best 
+# women wellness product manufactures provides 
+# good discounts  Is the problem , benefit and
+#  reasons to bleieve clear ?"""
+
+
+
+
+# step22 = """Earlier we shared three parts of a
+#  presentation. These are | We aim to solve this 
+#  "problem' |How to maintain self hygiene and 
+#  health during Pregnancy and Periods  
+#  The benefit we offer is |To provide
+#    discounted products for wellness  
+#    The reason to believe is | partnership with
+#      best women wellness product manufactures 
+#      provides good discounts  We want plan to 
+#      make this promise to people |We want plan to make this 
+#      promise to people |Care-free from periods and pregnancy no need to go to stores
+#        anymore order online products and get at your door save time and moneyWith minimum delivery charges and profits 
+#        from products soldIs this promise clear and compelling ?"""
+
+# step23="""Here are they key elements of our business pitch | PROBLEM - WHAT is the problem and for whom        
+# How to maintain self hygiene and health during Pregnancy and Periods
+#  PROMISE- In clear simple terms, what is the benefit you are offering      
+#      To provide discounted products for wellness PROOF - Why should we believe you can do this          
+#      partnership with best women wellness product manufactures provides good discounts PAYOFF - Dramatic Difference (how their life will be better/different)
+#      Care-free from periods and pregnancy no need to go to stores anymore order online products and get at your door save time and money
+#      PAYOFF - Dramatic Difference (how their life will be better/different)With minimum delivery charges and profits from products soldWith minimum delivery 
+#      charges and profits from products sold Based on this pitch is their clear evidence this company
+#        could makea profit ?"""
+
+
+# step3 ="""This is what we want people to do 
+# at the end of this talk | 
+# Please try our ecommerce site periods.co.in 
+# This is why they should do this |it is free to use 
+# and learn more about menstrual health and well-beingtry
+#  to register your self and use our free periods & 
+#  pregnancy tracking appGet suggestions based on it and 
+#  discounts on products Can anyone understand why they 
+#  should take this action ?"""
