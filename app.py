29c7951a598192a5f63cc45576ed5ee758c66678
taurtk <taurtk@gmail.com>
2024-01-03 16:43:17 +0530

    added dictionary approch

--- app.py
+++ app.py
@@ -42,29 +42,34 @@ def more():
 
 user_inputs = {
     'step1': {'input1': '', 'input2': '', 'input3': '', 'input4': ''},
-    'step2': {'input5': '', 'input6': '', 'input7': '', 'input8': ''},
-    'step3': {'input9': '', 'input10': '', 'input11': '', 'input12': ''},
-    'step4': {'input13': '', 'input14': '', 'input15': '', 'input16': ''},
+    'step2': {'input1': '', 'input2': '', 'input3': '', 'input4': ''},
+    'step3': {'input1': '', 'input2': '', 'input3': '', 'input4': '','input5':''},
+    'step4': {'input1': '', 'input2': ''},
 }
 
 
 @app.route('/generate_feedback', methods=['POST'])
 def generate_feedback():
     # Define a list to store user inputs
-    user_inputs = [request.form[f'user_input{i}'] for i in range(1, 5)]
+    user_inputs.setdefault('step1', {})
+
+# Update 'step1' with user inputs
+    for i in range(1, 5):
+        input_key = f'input{i}'
+        user_inputs['step1'][input_key] = request.form[f'user_input{i}']
     # try:
-   # Code that might raise a ZeroDivisionError    
-    pitch_input1 =user_input = user_inputs[0]
-    pitch_input2 =user_input2 = user_inputs[1]
-    pitch_input3 =user_input3 = user_inputs[2]
-    pitch_input4 =user_input4 = user_inputs[3]
-    print('i am here',pitch_input1)
+#    # Code that might raise a ZeroDivisionError    
+#     pitch_input1 =user_input = user_inputs[0]
+#     pitch_input2 =user_input2 = user_inputs[1]
+#     pitch_input3 =user_input3 = user_inputs[2]
+#     pitch_input4 =user_input4 = user_inputs[3]
+    # print('i am here',pitch_input1)
 
     step11 = f"""Here are some notes on a presentation we are preparing. We think people believe the following now:
-    |{user_input3} After our talk we want them to believe
-    |{user_input4}
+    |{user_inputs['step1']['input3']} After our talk we want them to believe
+    |{user_inputs['step1']['input4']}
     The end objective of this pitch is
-    |{user_input2} Based on these notes does it appear we have a clear objective for our presentation"""
+    |{user_inputs['step1']['input2']} Based on these notes does it appear we have a clear objective for our presentation"""
         # # Loop through prompts and get responses from ChatGPT
     for prompt in prompts_for_analysis():
             # Call OpenAI API
@@ -87,16 +92,21 @@ def generate_feedback():
 @app.route('/generate_feedback1', methods=['POST'])
 def generate_feedback1():
     # Define a list to store user inputs
-    user_inputs = [request.form[f'user_input{i}'] for i in range(1, 5)]
+    user_inputs.setdefault('step2', {})
+
+# Update 'step1' with user inputs
+    for i in range(1, 5):
+        input_key = f'input{i}'
+        user_inputs['step2'][input_key] = request.form[f'user_input{i}']
     # try:
    # Code that might raise a ZeroDivisionError    
-    pitch_input5 = user_input = user_inputs[0]
-    pitch_input6 =user_input2 = user_inputs[1]
-    pitch_input7 = user_input3 = user_inputs[2]
-    pitch_input8 =user_input4 = user_inputs[3]
+    # pitch_input5 = user_input = user_inputs[0]
+    # pitch_input6 =user_input2 = user_inputs[1]
+    # pitch_input7 = user_input3 = user_inputs[2]
+    # pitch_input8 =user_input4 = user_inputs[3]
 
     step12 = f"""Here is the introduction of a pitch
-    we are planning to make | {user_input} by {user_input2}  {user_input3} {user_input4} | Is this introduction clear, compelling and interesting ?
+    we are planning to make | {user_inputs['step2']['input1']} by {user_inputs['step2']['input2']}  {user_inputs['step2']['input3']} {user_inputs['step2']['input4']} | Is this introduction clear, compelling and interesting ?
     """
         # # Loop through prompts and get responses from ChatGPT
     for prompt in prompts_for_analysis():
@@ -120,29 +130,31 @@ def generate_feedback1():
 @app.route('/generate_feedback2', methods=['POST'])
 def generate_feedback2():
     # Define a list to store user inputs
-    user_inputs = [request.form[f'user_input{i}'] for i in range(1, 6)]
+    for i in range(1, 6):
+        input_key = f'input{i}'
+        user_inputs['step3'][input_key] = request.form[f'user_input{i}']
     # try:
    # Code that might raise a ZeroDivisionError    
-    pitch_input8 =user_input = user_inputs[0]
-    pitch_input9 =user_input2 = user_inputs[1]
-    pitch_input10 =user_input3 = user_inputs[2]
-    pitch_input11 =user_input4 = user_inputs[3]
-    pitch_input12 =user_input5 = user_inputs[4]
+    # pitch_input8 =user_input = user_inputs[0]
+    # pitch_input9 =user_input2 = user_inputs[1]
+    # pitch_input10 =user_input3 = user_inputs[2]
+    # pitch_input11 =user_input4 = user_inputs[3]
+    # pitch_input12 =user_input5 = user_inputs[4]
 
     
     step2 =f"""
 
  Here are they key elements of our business pitch
    | PROBLEM - WHAT is the problem and for whom    
-    {user_input} PROMISE- In clear simple terms,
+    {user_inputs['step3']['input1']} PROMISE- In clear simple terms,
    what is the benefit you are offering        
-   {user_input2} PROOF 
+   {user_inputs['step3']['input2']} PROOF 
    - Why should we believe you can do this         
-     {user_input3} PAYOFF
+     {user_inputs['step3']['input3']} PAYOFF
          - Dramatic Difference (how their life will be
-           better/different){user_input4}PAYOFF - Dramatic Difference 
+           better/different){user_inputs['step3']['input4']}PAYOFF - Dramatic Difference 
                (how their life will be better/different)
-               {user_input5}
+              {user_inputs['step3']['input5']}
                Based on this pitch is their clear
                  evidence this company could makea profit ?"""
 
@@ -168,17 +180,19 @@ def generate_feedback2():
 @app.route('/generate_feedback3', methods=['POST'])
 def generate_feedback3():
     # Define a list to store user inputs
-    user_inputs = [request.form[f'user_input{i}'] for i in range(1, 3)]
+    for i in range(1, 3):
+        input_key = f'input{i}'
+        user_inputs['step4'][input_key] = request.form[f'user_input{i}']
     # try:
    # Code that might raise a ZeroDivisionError    
-    pitch_input13 =user_input = user_inputs[0]
-    pitch_input14 = user_input2 = user_inputs[1]
+    # pitch_input13 =user_input = user_inputs[0]
+    # pitch_input14 = user_input2 = user_inputs[1]
     
 
     step3= f"""This is what we want people to do at the end of this talk
-      | {user_input}
+      | {user_inputs['step4']['input1']}
       This is why they should do this 
-      |{user_input2}
+      |{user_inputs['step4']['input2']}
         Can anyone understand why they should take this action ?"""
         # # Loop through prompts and get responses from ChatGPT
     for prompt in prompts_for_analysis():
@@ -206,7 +220,7 @@ def generate_pitch():
 #    # Code that might raise a ZeroDivisionError    
 #     user_input = user_inputs[0]
 #     user_input2 = user_inputs[1]
-    print('i amhere ',pitch_input1)
+    # print('i amhere ',pitch_input1)
     pitch_prompt= f"""We have been working on 
           pitch and need help with a script for the pitch 
 
@@ -216,9 +230,9 @@ def generate_pitch():
     Questions asked are shown first our replies follow after a | symbol 
 
     Please write a script for a talk based on this content 
-    Write the talk as a real talk without speaker notes Why is this pitch important to you? | {pitch_input1}##What is the goal of your pitch? |{pitch_input2} ##What does your audience believe before the pitch?         | {pitch_input3}##What should your audience  believe after the pitch?      (and Why) | {pitch_input4} ##Note: What happens in between is your pitch...  | ##In one line, what is your pitch? 
-    {pitch_input5} ##Up to 3 supporting arguments Argument 1 | {pitch_input6}## Argument 2 | {pitch_input7}## Argument 3 |{pitch_input8}####Here is core content of  pitch##PROBLEM - WHAT is the problem and for whom         | {pitch_input9} ##PROMISE- In clear simple terms, what is the benefit you are offering         | {pitch_input10} ##PROOF - Why should we believe you can do this          |  {pitch_input11} ##PAYOFF - Dramatic Difference (how their life will be better/different) | {pitch_input12}##PROFIT (If applicable) - how will this product/service make a profit | {pitch_input13}##PASSION - Why are you excited about this | {pitch_input14}####What follows  is the content for the last slide of the presentation##Call to action (what after your pitch) -
-    (1 Sentence)  [Try to include what they will be saying yes to in concrete terms] | {pitch_input15}##Why do this (up to 3 reasons)  Note: Include why do this Now Reason | {pitch_input16}##Your last line
+    Write the talk as a real talk without speaker notes Why is this pitch important to you? | {user_inputs['step1']['input1']}##What is the goal of your pitch? |{user_inputs['step1']['input2']} ##What does your audience believe before the pitch?         | {user_inputs['step1']['input3']}##What should your audience  believe after the pitch?      (and Why) | {user_inputs['step1']['input4']} ##Note: What happens in between is your pitch...  | ##In one line, what is your pitch? 
+    {user_inputs['step2']['input1']} ##Up to 3 supporting arguments Argument 1 |{user_inputs['step2']['input2']}## Argument 2 | {user_inputs['step2']['input3']}## Argument 3 |{user_inputs['step2']['input4']}####Here is core content of  pitch##PROBLEM - WHAT is the problem and for whom         | {user_inputs['step3']['input1']}  ##PROOF - Why should we believe you can do this          |  {user_inputs['step3']['input2']} ##PAYOFF - Dramatic Difference (how their life will be better/different) | {user_inputs['step3']['input3']}##PROFIT (If applicable) - how will this product/service make a profit | {user_inputs['step3']['input4']}##PASSION - Why are you excited about this |{user_inputs['step3']['input5']}####What follows  is the content for the last slide of the presentation##Call to action (what after your pitch) -
+    (1 Sentence)  [Try to include what they will be saying yes to in concrete terms] | {user_inputs['step4']['input1']}##Why do this (up to 3 reasons)  Note: Include why do this Now Reason | {user_inputs['step4']['input2']}##Your last line
     What do they need to remember /do |  
             | note |
             Please remove placeholders from the final pitch"""
@@ -235,10 +249,10 @@ def generate_pitch():
             # Append the response to the list
         feedback =  response.choices[0].message.content
         # print(feedback)
-        print('i amhere ',pitch_input1)
+        # print('i amhere ',pitch_input1)
 
         
-    return render_template('pitch.html', user_inputs=pitch_input1, feedback=feedback)
+    return render_template('pitch.html', user_inputs=user_inputs, feedback=feedback)
 
 
 
